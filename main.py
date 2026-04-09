09701b7eb08c6886035b1e5acfceae9d92a570e1
Frozenhood <shavarin.o@gmail.com>
2026-04-09 19:04:24 +0200

    Replace requests with cloudscraper for enhanced web scraping functionality and update requirements.txt accordingly.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 import json
 import os
@@ -10,18 +10,11 @@ DATA_DIR = "data"
 REPORT_DIR = "reports"
 HISTORY_FILE = "price-history.json"
 
-def fetch_ads_from_page(page):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'sr-RS,sr;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-    }
+scraper = cloudscraper.create_scraper()
 
+def fetch_ads_from_page(page):
     url = f"{FILTER_URL}&page={page}"
-    response = requests.get(url, headers=headers, timeout=10)
+    response = scraper.get(url, timeout=15)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'html.parser')
